5a5c032a39d700096140e92664c593e2a114cd49
Bryan Ortiz <bryan.ortiz@holinsys.pe>
2021-12-29 03:09:48 -0500

    Busca Usuario addedd

--- src/api/user/routes/user.intranet.routes.ts
+++ src/api/user/routes/user.intranet.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express'
-import {  findAllUsersController, findAllUsersRecordTypeController, findAllUsersRangeAgeController, UnsubscribeTheUserAndClean } from '../controllers/user.controller'
+import {
+  findAllUsersController,
+  findAllUsersRecordTypeController,
+  findAllUsersRangeAgeController,
+  UnsubscribeTheUserAndClean,
+  SeachUsersController,
+} from '../controllers/user.controller'
 import { listUserIntranetValidator, listUserRecordType, UnsubscribeUser } from '../middlewares/user.validator'
 export const router: Router = Router()
 router.get('/', listUserIntranetValidator, findAllUsersController)
-
+router.get('/search/:q', SeachUsersController)
 router.get('/recordType', listUserRecordType, findAllUsersRecordTypeController)
 
 router.get('/rangeAge', findAllUsersRangeAgeController)
 
-
-router.post('/unsubscribe/clean',UnsubscribeUser, UnsubscribeTheUserAndClean)
+router.post('/unsubscribe/clean', UnsubscribeUser, UnsubscribeTheUserAndClean)
 
 // router.post('/activate', ActiveAccountUserValidator, ActiveAccountUserController)
-
--- src/api/user/services/find/index.ts
+++ src/api/user/services/find/index.ts
@@ -1,6 +1,6 @@
 import { DataBase } from '../../../../database/index'
 import { UserAttributes } from '../../models/user.model'
-import sequelize,{ Op, WhereOptions } from 'sequelize'
+import sequelize, { Op, WhereOptions } from 'sequelize'
 import { FindAttributeOptions, Order } from 'sequelize/types'
 
 export const findUserByEmail = async ({ email }: { email: string }): Promise<UserAttributes> => {
@@ -153,6 +153,58 @@ export const findAllUsers = async ({
   }
 }
 
+export const SearchUser = async ({ regex, order }: { regex?: string; order: Order }) => {
+  try {
+    const limit: number = 12
+    const users = await DataBase.instance.user.findAll({
+      where: {
+        state: true,
+        [Op.or]: {
+          id: {
+            [Op.regexp]: regex,
+          },
+          name: {
+            [Op.regexp]: regex,
+          },
+          dni: {
+            [Op.regexp]: regex,
+          },
+          lastname: {
+            [Op.regexp]: regex,
+          },
+        },
+      },
+      attributes: {
+        exclude: [
+          'password',
+          'salt',
+          'status',
+          'numIntentos',
+          'fechaFinBloqueo',
+          'hora_bloqueo',
+          'cantidad_min_bloqueado',
+          'nightmode',
+          'state',
+          'device_id',
+          'ext',
+          'path',
+          'number_of_sessions',
+          'updated',
+          'created',
+          'code_verification',
+          'size',
+          'filename',
+          'date_user_session_day',
+        ],
+      },
+      order,
+      limit,
+    })
+    return users
+  } catch (err) {
+    throw err
+  }
+}
 
 export const findAllUsersRecordType = async ({
   where,
@@ -177,21 +229,24 @@ export const findAllUsersRecordType = async ({
   }
 }
 
-
-export const findAllUsersRangeAge = async (
-) => {
+export const findAllUsersRangeAge = async () => {
   try {
     const listRangeAge = await DataBase.instance.user.findAll({
       attributes: [
-        [sequelize.literal(`CASE WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) < 20 THEN 'I) <20' WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 20 AND 29 THEN 'II) 20-29' 
+        [
+          sequelize.literal(`CASE WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) < 20 THEN 'I) <20' WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 20 AND 29 THEN 'II) 20-29' 
         WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 30 AND 39 THEN 'III) 30-39' WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 40 AND 49 THEN 'IV) 40-49' 
         WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 50 AND 59 THEN 'V) 50-59' WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 60 AND 69 THEN 'VI) 60-69' 
-        WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 70 AND 79 THEN 'VII) 70-79' ELSE 'VIII) 80>' END`), 'range_age'],
-        [sequelize.literal(`COUNT(CASE WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) < 20 THEN 'I) <20' WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 20 AND 29 THEN 'II) 20-29' 
+        WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 70 AND 79 THEN 'VII) 70-79' ELSE 'VIII) 80>' END`),
+          'range_age',
+        ],
+        [
+          sequelize.literal(`COUNT(CASE WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) < 20 THEN 'I) <20' WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 20 AND 29 THEN 'II) 20-29' 
         WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 30 AND 39 THEN 'III) 30-39' WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 40 AND 49 THEN 'IV) 40-49' 
         WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 50 AND 59 THEN 'V) 50-59' WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 60 AND 69 THEN 'VI) 60-69' 
-        WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 70 AND 79 THEN 'VII) 70-79' ELSE 'VIII) 80>' END)`), 'cantidad'],
-        
+        WHEN TIMESTAMPDIFF(YEAR, date_of_birth , CURRENT_TIMESTAMP()) BETWEEN 70 AND 79 THEN 'VII) 70-79' ELSE 'VIII) 80>' END)`),
+          'cantidad',
+        ],
       ],
       order: [[sequelize.literal('range_age'), 'ASC']],
       group: ['range_age'],
@@ -214,7 +269,7 @@ export const findAllUsersReport = async ({
     const users = await DataBase.instance.user.findAll({
       where,
       attributes,
-      logging:console.log
+      logging: console.log,
     })
     let copy_list = JSON.parse(JSON.stringify(users))
     return copy_list
